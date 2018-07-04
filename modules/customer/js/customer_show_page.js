a2b7b81a1c376cd490f09d78a6c005b901c31d9c
gn301046 <1104137110@gm.kuas.edu.tw>
2018-07-04 07:28:16 +0000

    a

--- modules/customer/js/customer_show_page.js
+++ modules/customer/js/customer_show_page.js
@@ -10,10 +10,10 @@ class customer_show_page extends ActionHandler{
 					<span class="content_header"> 客戶資料</span>
 					</br>
 					<button class="btn btn-primary theme-style" id="berth_customer">停泊客戶</button>
-					<button class="btn btn-primary theme-style" id="berth_customer">租賃客戶</a>
-					<button class="btn btn-primary theme-style" id="berth_customer">陸域租戶</a>
-					<button class="btn btn-primary theme-style" id="berth_customer">維修客戶</a>
-				    <button class="btn btn-primary theme-style" id="berth_customer">下水碼頭客戶</a>
+					<button class="btn btn-outline-secondary" id="berth_customer">租賃客戶</a>
+					<button class="btn btn-outline-secondary" id="berth_customer">陸域租戶</a>
+					<button class="btn btn-outline-secondary" id="berth_customer">維修客戶</a>
+				    <button class="btn btn-outline-secondary" id="berth_customer">下水碼頭客戶</a>
 		`
 		$(this.position_id).html(html);
 
