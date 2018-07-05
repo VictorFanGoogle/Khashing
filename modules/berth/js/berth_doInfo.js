30ca8c3b307089b89a649e30d0f71a265646a3da
gn306029 <gn306029@gmail.com>
2018-07-05 01:39:09 +0000

    add berth_doInfo function

--- modules/berth/js/berth_doInfo.js
+++ modules/berth/js/berth_doInfo.js
@@ -6,6 +6,14 @@ class berth_doInfo extends ActionHandler{
 	}
 	ajaxSuccess(data){
 	    console.log(data)
+	    let view = `
+	    	<div style='position:relative;width:100%;background-color:BLACK;'>
+	    		<div style='position:absolute;top:20%;left:20%;width:10%;background-color:RED;'>
+	    		</div>
+	    	</div>
+	    `;
+	    console.log(this.position_id)
+	    $("#"+this.position_id).html(view);
 	    return data;
 	}
 	showResult(){}
